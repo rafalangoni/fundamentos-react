a835e3cf2ffbd6029b11b9175117583d08727c42
Rafael Langoni <langoninaves@gmail.com>
2022-08-25 16:58:41 -0300

    primeiro componente

---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App(){
+    const mensagem = 'Seja bem-vindo!'
+    return(
+        <div>
+            <h2>Primeiro componente</h2>
+            <p>{mensagem}</p>
+        </div>
+
+    )
+}
