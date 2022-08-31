837a1b6a441b6df2d0a4c7b11c20bf480ceae766
Rafael Langoni <langoninaves@gmail.com>
2022-08-31 16:54:06 -0300

    criando chave para o filho

--- src/components/basicos/Familia.jsx
+++ src/components/basicos/Familia.jsx
@@ -1,10 +1,11 @@
 import React, {cloneElement} from "react";
 
 const Familia = (props) => {
+
   return (
     <div>
-        {props.children.map((child) =>{
-            return cloneElement(child, {...props})
+        {props.children.map((child, i) =>{
+            return cloneElement(child, {...props, key: i})
         })}
     </div>
   );
