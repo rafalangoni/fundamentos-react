6befa8fc488ed8cc9a1d2f5c711135268eae7217
Rafael Langoni <langoninaves@gmail.com>
2022-09-07 14:46:58 -0300

    useState

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-const IndiretaFilho = (props) =>{
-const passarInformacoes = () =>{
-    props.quandoClicar('Lucas', 40, true)
-}
+const IndiretaFilho = (props) => {
+  const passarInformacoes = () => {
+    const idade = parseInt(Math.random() * (150 - 18) - 18);
+    const nerd = Math.random() > 0.5;
+    props.quandoClicar("Lucas", idade, nerd);
+  };
 
-    return (
-        <div>
-           <div>Filho</div>
-           <button onClick={passarInformacoes}>Fornecer informacoes para o componente pai</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>Filho</div>
+      <button onClick={passarInformacoes}>
+        Fornecer informacoes para o componente pai
+      </button>
+    </div>
+  );
+};
 
 export default IndiretaFilho;
--- src/components/comunicacao/IndiretaPai.jsx
+++ src/components/comunicacao/IndiretaPai.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 const IndiretaPai = () => {
+  const [nome, setNome] = useState("Anônimo");
+  const [idade, setIdade] = useState(0);
+  const [nerd, setNerd] = useState(false);
+
   function fornecerInformacoes(nome, idade, nerd) {
-    console.log(nome, idade, nerd);
+    setNome(nome)
+    setIdade(idade)
+    setNerd(nerd)
   }
 
   return (
     <div>
-      <div>Pai</div>
+      <div>
+        <span>{nome} </span>
+        <span>{idade} </span>
+        <span>{nerd ? "Verdadeiro" : "Falso"}</span>
+      </div>
       <div>
         <IndiretaFilho quandoClicar={fornecerInformacoes} />
       </div>
