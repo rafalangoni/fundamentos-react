8e2cea6e5dd4e194a86edfb70e7169596fa5bb7d
Rafael Langoni <langoninaves@gmail.com>
2022-09-18 14:38:32 -0300

    contador funcionando com passo

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -3,18 +3,27 @@ import { useState } from "react";
 
 const Contador = () => {
   const [valor, setValor] = useState(0);
+  const [passo, setPasso] = useState(1)
 
   const adicionar = () => {
-    setValor(valor + 1);
+    setValor(valor + passo);
   };
 
   const diminuir = () => {
-    setValor(valor - 1);
+    setValor(valor - passo);
+  };
+
+  const usaPasso = (evento) => {
+    setPasso(parseInt(evento.target.value))
   };
 
   return (
     <div>
-      <h3>Contador</h3>
+      <h3>
+        <strong>Contador</strong>
+      </h3>
+      <label htmlFor="inputPasso">Passo </label>
+      <input onChange={usaPasso} type="number" id="inputPasso"></input>
       <p>
         Valor: <span>{valor}</span>
       </p>
